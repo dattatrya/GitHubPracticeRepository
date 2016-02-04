c91008f789d248f29b6ad2236c254ee937bbb018
dattatrya <datta321@gmail.com>
2016-02-04 18:13:58 +0530

    Added MVC practice project

---
+++ MVCBlankWebAPP/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBlankWebAPP.Controllers
+{
+    public class HelloController : Controller
+    {
+        //public void Execute(System.Web.Routing.RequestContext requestContext)
+        //{
+        //    requestContext.HttpContext.Response.Write("Hello World");
+                     
+        //}
+
+        public string SayHello(int? sample)
+        {
+            return "Hello world! V2" + sample.ToString();
+        }
+    }
+}
---
+++ MVCBlankWebAPP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCBlankWebAPP.MvcApplication" Language="C#" %>
